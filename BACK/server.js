9aa2b56770bd643b3cef441c370a8f749fe5cbac
GNT <gastonnt123@gmail.com>
2023-06-22 15:44:56 -0300

    as

--- BACK/server.js
+++ BACK/server.js
@@ -56,7 +56,7 @@ const transporter = nodemailer.createTransport({
   });
   
 
-const port = 3001;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
