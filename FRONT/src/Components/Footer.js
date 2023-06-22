89dc21ec026b1b18d9b53b1f3dbd99db393122e2
GNT <gastonnt123@gmail.com>
2023-06-22 15:49:21 -0300

    as

--- FRONT/src/Components/Footer.js
+++ FRONT/src/Components/Footer.js
@@ -24,12 +24,7 @@ class Footer extends Component {
 
               <ul className="copyright">
                 <li>&copy; Gaston Terminiello</li>
-                <li>
-                  Design by{" "}
-                  <a title="Styleshout" href="http://www.styleshout.com/">
-                    Styleshout
-                  </a>
-                </li>
+               
               </ul>
             </div>
           </Fade>
